2e6fa622f6a769b2d18a89921800c819bad3e956
otienosteve <steveotieno701@gmail.com>
2022-07-19 19:49:03 +0300

    commit tree 907c3a57df97d6fd4faac86e63cf140181dc2ba6

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
+require "pry"
 class Triangle
-  # write code here
+  attr_reader :equilateral,:isosceles,:scalene
+   def initialize(side1,side2,side3)
+   
+@side1=side1
+@side2=side2
+@side3=side3
+   end
+
+  def kind
+    sides=[@side1,@side2,@side3].sort
+  
+  if @side1==0&&@side2==0&&@side3==0
+    raise TriangleError
+  elsif @side1<0||@side2<0||@side3<0
+      raise TriangleError
+  elsif sides[0]+sides[1]<=sides[2]
+    raise TriangleError
+  elsif @side1==@side2 && @side1==@side3
+  :equilateral
+  elsif @side1==@side2 || @side3 ==@side1 ||@side2==@side3
+  :isosceles
+  else
+  :scalene
+  end
+  # binding.pry
+  end
+
+              class TriangleError < StandardError
+              # binding.pry
+              def message
+                "for triangles with no size!"
+              end
+              end
 end
+t1=Triangle.new(3,4,5)
